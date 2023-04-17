d5d7a5e388ee01cb5fabf69a715a494d7f74a824
Gujinn <theo.nairi@epitech.eu>
2023-04-17 09:42:11 +0200

    [+] improve sign lab repo

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Mailgo</title>
-                <meta name='description' content="Gain control of your business's growth with Mailgo's comprehensive marketing, automation, and email marketing platform." />
+                <title>Sign Lab</title>
+                <meta name='description' content="Faciliter l'échange autour de la langue des signes" />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
