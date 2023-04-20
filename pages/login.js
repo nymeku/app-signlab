b529d6368fca2940ae8480cfcef54c395121f532
Gujinn <theo.nairi@epitech.eu>
2023-04-20 14:40:16 +0200

    [+] improve login page

--- pages/login.js
+++ pages/login.js
@@ -9,20 +9,14 @@ export default function Login() {
 	return (
 		<>
 			<Head>
-				<title>Login - Signlab</title>
+				<title>Connexion - Signlab</title>
 			</Head>
 			<main className="w-full h-screen flex flex-col items-center justify-center px-4">
 				<div className="max-w-sm w-full text-gray-300">
 					<div className="text-center">
 						<Brand className="mx-auto w-32" />
 						<div className="mt-5 space-y-2">
-							<h1 className="text-white text-2xl font-bold sm:text-3xl">Log in to your account</h1>
-							<p className="">
-								Don't have an account?{" "}
-								<Link href="/#pricing" className="font-medium text-purple-500 hover:text-purple-600 duration-150">
-									Get access
-								</Link>
-							</p>
+							<h1 className="text-white text-2xl font-bold sm:text-2xl">Se connecter à mon compte</h1>
 						</div>
 					</div>
 					<form onSubmit={(e) => e.preventDefault()} className="mt-8 space-y-5">
@@ -34,15 +28,16 @@ export default function Login() {
 							<label className="font-medium">Password</label>
 							<Input type="password" required className="w-full mt-2 text-gray-300 bg-gray-800 focus:bg-gray-900 focus:border-gray-800" />
 						</div>
-						<Button className="w-full text-gray-800 bg-gray-100 hover:bg-gray-200 ring-offset-2 focus:ring rounded-lg">Sign in</Button>
-						<button
-							type="button"
-							className="w-full flex items-center justify-center gap-x-3 py-2.5 border border-gray-800 rounded-lg text-sm font-medium bg-gray-800/40 hover:bg-gray-800 ring-purple-500 focus:ring duration-150"
-						>
-							<GoogleIcon />
-							Continue with Google
-						</button>
+						<Button className="w-full text-gray-800 bg-gray-100 hover:bg-gray-200 ring-offset-2 focus:ring rounded-lg">Connexion</Button>
 					</form>
+					<div>
+					<p className="text-center mt-3">
+								Pas de compte ?{" "}
+								<Link href="/register" className="font-medium text-purple-500 hover:text-purple-600 duration-150">
+									S'inscrire
+								</Link>
+					</p>
+					</div>
 				</div>
 			</main>
 		</>
