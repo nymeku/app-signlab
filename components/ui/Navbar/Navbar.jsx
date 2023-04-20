dfbf0cdd8ce62eba030047ab643f8f964c7e1ca9
nymeku <arnaud.andrieu@epitech.eu>
2023-04-20 17:14:21 +0200

    [+] adapt navbar

--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -2,10 +2,15 @@ import Link from "next/link"
 import { useEffect, useRef, useState } from "react"
 import NavHeader from "../NavHeader"
 import NavLink from "../NavLink"
+import React from "react"
+import { HomeContext } from "@/pages"
+import { Avatar, Button } from "@chakra-ui/react"
+import { useRouter } from "next/router"
 
-const Navbar = ({ user }) => {
+const Navbar = () => {
 	const [state, setState] = useState(false)
 	const menuBtnEl = useRef()
+	const { user } = React.useContext(HomeContext)
 
 	const navigation = [
 		{ name: "Fonctionnalités", href: "/#features" },
@@ -13,6 +18,7 @@ const Navbar = ({ user }) => {
 		{ name: "Avis", href: "/#testimonials" },
 		{ name: "FAQs", href: "/#faqs" },
 	]
+	const router = useRouter()
 
 	useEffect(() => {
 		document.onclick = (e) => {
@@ -40,24 +46,36 @@ const Navbar = ({ user }) => {
 									</li>
 								)
 							})}
+							<li>
+								<Button colorScheme="purple" onClick={() => router.push("/app")}>
+									Traduire
+								</Button>
+							</li>
 						</ul>
 						<div className="gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
-							<Link href="/login" className="block hover:text-gray-50">
-								Se connecter
-							</Link>
-							<NavLink
-								href="#pricing"
-								className="flex items-center justify-center gap-x-1 text-sm text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex"
-							>
-								Nos offres
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-									<path
-										fillRule="evenodd"
-										d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-										clipRule="evenodd"
-									/>
-								</svg>
-							</NavLink>
+							{user ? (
+								<Avatar name={"Arnaud Andrieu"} />
+							) : (
+								<>
+									<Link href="/login" className="block hover:text-gray-50">
+										Se connecter
+									</Link>
+
+									<NavLink
+										href="#pricing"
+										className="flex items-center justify-center gap-x-1 text-sm text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex"
+									>
+										Nos offres
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+											<path
+												fillRule="evenodd"
+												d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+												clipRule="evenodd"
+											/>
+										</svg>
+									</NavLink>
+								</>
+							)}
 						</div>
 					</div>
 				</div>
