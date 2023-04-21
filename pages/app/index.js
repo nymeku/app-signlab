d1e2eb481399ac32c47144a12e44781648718322
Gujinn <theo.nairi@epitech.eu>
2023-04-21 11:41:50 +0200

    [+] icons

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,9 +1,22 @@
 import AppLayout from "@/components/AppLayout"
-
-const { default: Sidebar } = require("@/components/Sidebar/sidebar")
+import { getUserBySession } from "@/models/user.model"
+import { getSession } from "next-auth/react"
 
 const MainApp = () => {
 	return <AppLayout></AppLayout>
 }
 
 export default MainApp
+
+export const getServerSideProps = async (context) => {
+	const session = await getSession(context)
+	if (!session) return { redirect: { destination: '/login', permanent: false } }
+	const user = await getUserBySession(session)
+	if (!user) return { redirect: { destination: '/login', permanent: false } }
+	
+	return {
+		props: {
+			user: JSON.stringify(user)
+		}
+	}
+}
