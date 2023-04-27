55d3bd5996e3fe5b6cef29c098e98d517d8654ab
nymeku <arnaud.andrieu@epitech.eu>
2023-04-27 15:15:37 +0200

    [+] finished video recorder

--- pages/app/traduction/index.js
+++ pages/app/traduction/index.js
@@ -1,45 +1,50 @@
 import AppLayout from "@/components/AppLayout"
-import { Box, Heading, Button } from "@chakra-ui/react"
+import { Box, Heading, Button, ButtonGroup } from "@chakra-ui/react"
 import React from "react"
 
 const Translation = () => {
 	const launchVideo = () => {
-        var video = document.querySelector("#video");
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-                .then(function (stream) {
-                    video.srcObject = stream;
-                })
-                .catch(function (err0r) {
-                    console.log("Something went wrong!");
-                });
-        }
-    }
+		var video = document.querySelector("#video")
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices
+				.getUserMedia({ video: true })
+				.then(function (stream) {
+					video.srcObject = stream
+				})
+				.catch(function (err0r) {
+					console.log("Something went wrong!")
+				})
+		}
+	}
 
 	const stopVideo = () => {
-		var video = document.querySelector("#video");
-		video.srcObject.getTracks().forEach(function (track) {
-			track.stop();
-		});
-		video.srcObject = null;
+		var video = document.querySelector("#video")
+		if (video.srcObject) {
+			video.srcObject.getTracks().forEach(function (track) {
+				track.stop()
+			})
+		}
+		video.srcObject = null
 	}
 
 	return (
 		<AppLayout>
-			<Box>
-				<Heading fontSize={"lg"}>Nouvelle traduction</Heading>
+			<Heading fontSize={"lg"}>Nouvelle traduction</Heading>
 
-				<video id="video" width="320" height="240" autoPlay></video>
+			<Box display={"flex"} justifyContent={"center"} flexDirection={"column"} alignItems={"center"}>
+				<Box transform={"scaleX(-1)"} display={"flex"} justifyContent={"center"} marginBottom={"20px"}>
+					<video id="video" width="700" height="700" autoPlay></video>
+				</Box>
 
-				
+				<ButtonGroup>
 					<Button colorScheme={"red"} id="stop-record" onClick={() => stopVideo()}>
 						Stop
 					</Button>
-		
+
 					<Button colorScheme="purple" id="start-record" onClick={() => launchVideo()}>
 						Démarrer
 					</Button>
-	
+				</ButtonGroup>
 			</Box>
 		</AppLayout>
 	)
